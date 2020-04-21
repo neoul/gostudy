4201b18a12cd0e39701e79e04b316e2dbb4d2d9e
neoul <neoul@ymail.com>
2020-04-21 13:39:21 +0900

    Updated reflection.go

--- datablock/main.go
+++ datablock/main.go
@@ -4,9 +4,15 @@ import (
 	"os"
 
 	"github.com/neoul/gostudy/datablock/log"
+	yref "github.com/neoul/gostudy/datablock/reflection"
 )
 
+
 func main() {
-	l := log.NewLog("hello", os.Stdout)
-	l.Debug("DEBUG message")
+	// var i int
+	var ri int
+	a := 1
+	ylog := log.NewLog("main", os.Stdout)
+	ri = yref.NewValue(a, 10).(int)
+	ylog.Debug(yref.PrintValue(ri))
 }
